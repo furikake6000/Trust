4c5ef52b9c9f3e79791426d625e59a3a596fdadf
furikake6000 <ewave63@keio.jp>
2019-09-05 00:08:46 +0900

    Success Rateの表示

--- app/controllers/static_pages.go
+++ app/controllers/static_pages.go
@@ -1,9 +1,28 @@
 package controllers
 
 import (
+	"math"
+	"math/rand"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	robotSuccessRate float64
+	humanSuccessRate float64
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+	robotSuccessRate = rand.Float64()
+	humanSuccessRate = rand.Float64()
+}
+
 func Index(c *gin.Context) {
-	c.HTML(200, "index.html", gin.H{})
+	c.HTML(200, "index.html", gin.H{
+		"humanSuccessRate": math.Ceil(humanSuccessRate * 100),
+		"robotSuccessRate": math.Ceil(robotSuccessRate * 100),
+	})
 }
