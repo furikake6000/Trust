b75986842c826acd29ac9ed1288327678abe2254
furikake6000 <ewave63@keio.jp>
2019-09-05 01:34:12 +0900

    相互選択システムと過信・不信のしくみを作成

--- app/controllers/static_pages.go
+++ app/controllers/static_pages.go
@@ -9,24 +9,43 @@ import (
 )
 
 var (
-	robotSuccessRate float64
-	humanSuccessRate float64
+	robotSuccessRate    float64
+	humanSuccessRate    float64
+	mutualSelectionMode bool
+	isHumansTurn        bool
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 	reset()
+	mutualSelectionMode = false
+	isHumansTurn = true
 }
 
 func reset() {
-	robotSuccessRate = rand.Float64()
-	humanSuccessRate = rand.Float64()
+	humanSuccessRate = rand.Float64()*0.4 + 0.3
+	if humanSuccessRate > 0.5 {
+		robotSuccessRate = humanSuccessRate + rand.Float64()*0.2 + 0.1
+	} else {
+		robotSuccessRate = humanSuccessRate - rand.Float64()*0.2 - 0.1
+	}
+	isHumansTurn = true
 }
 
 func Index(c *gin.Context) {
+	var robotsSelection = -1
+	if mutualSelectionMode && !isHumansTurn {
+		if robotSuccessRate > humanSuccessRate {
+			robotsSelection = 1
+		} else {
+			robotsSelection = 0
+		}
+	}
 	c.HTML(200, "index.html", gin.H{
-		"humanSuccessRate": math.Ceil(humanSuccessRate * 100),
-		"robotSuccessRate": math.Ceil(robotSuccessRate * 100),
+		"humanSuccessRate":    math.Ceil(humanSuccessRate * 100),
+		"robotSuccessRate":    math.Ceil(robotSuccessRate * 100),
+		"mutualSelectionMode": mutualSelectionMode,
+		"robotsSelection":     robotsSelection,
 	})
 }
 
@@ -36,8 +55,16 @@ func Reset(c *gin.Context) {
 	c.Redirect(302, "/")
 }
 
+func Switch(c *gin.Context) {
+	reset()
+	mutualSelectionMode = !mutualSelectionMode
+
+	c.Redirect(302, "/")
+}
+
 func Execute(c *gin.Context) {
 	c.Request.ParseForm()
+	isHumansTurn = !isHumansTurn
 	var isRobotPlay = c.Request.Form["isRobotPlay"][0] == "Yes"
 	var isSuccess bool
 	if isRobotPlay {
--- app/main.go
+++ app/main.go
@@ -20,6 +20,7 @@ func main() {
 	// Root
 	router.GET("/", controllers.Index)
 	router.GET("/reset", controllers.Reset)
+	router.GET("/switch", controllers.Switch)
 	router.POST("/execute", controllers.Execute)
 
 	router.Run(":8080")
